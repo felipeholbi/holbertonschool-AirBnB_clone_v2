d4875ba104ca39feec6d090f5f2526cc5b976c54
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-06 17:49:56 -0500

    Prepares a MySQL

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- Create a database in mysql
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create user in mysql
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Give privileges
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Give SELECT privileges 
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
+-- Update privileges
+FLUSH PRIVILEGES;
