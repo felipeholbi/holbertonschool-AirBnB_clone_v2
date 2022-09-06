2f13032f04fc8efcb8dfc88842ffe61b41c83914
felipeholbi <4758@holbertonstudents.com>
2022-09-06 17:38:48 -0500

    setup
    add coments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,9 @@
---This script setup a database
+-- This script setup a database
+-- Create db
 CREATE DATABASE IF NOT EXIST hbnb_dev_db;
+-- create user and passwd
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Privileges
 GRANT ALL PRIVILEGES ON * . * TO 'hbnb_dev'@'localhost';
+-- Privileges select
 GRANT SELECT ON performance_schema.* TO 'r1';
-sudo service mysql restart
