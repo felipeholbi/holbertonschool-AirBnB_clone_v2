930cdf7aef74ba78358cd0b55d65abd33182e97a
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-06 17:55:01 -0500

    Update do create

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,6 +5,6 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Give privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- Give SELECT privileges 
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 -- Update privileges
 FLUSH PRIVILEGES;
