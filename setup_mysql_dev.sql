4b0992f805b855ff786cf80f69f5a8d543450939
felipeholbi <4758@holbertonstudents.com>
2022-09-06 17:49:30 -0500

    fix setup_db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,8 +2,8 @@
 -- Create db
 CREATE DATABASE IF NOT EXIST hbnb_dev_db;
 -- create user and passwd
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Privileges
-GRANT ALL PRIVILEGES ON * . * TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Privileges select
 GRANT SELECT ON performance_schema.* TO 'r1';
