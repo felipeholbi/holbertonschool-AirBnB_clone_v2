6b492551e0b2c89276f2614a6f984e051c952d96
felipeholbi <4758@holbertonstudents.com>
2022-09-06 18:08:46 -0500

    fix user_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,3 +7,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- Give SELECT privileges
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Update privileges
