62526aa1921708bea11aa17775bed10ec3171c1e
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-06 17:53:09 -0500

    Update do create

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- Create a database in mysql
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_tets_db;
 -- Create user in mysql
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Give privileges
