89d1f93acaaeed1badb8c4be5ad19feae51ddbcf
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-08 18:19:50 -0500

    Task 10

--- models/place.py
+++ models/place.py
@@ -30,32 +30,31 @@ place_amenity = Table(
 
 class Place(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db") else object):
     """ A place to stay"""
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "places"
+    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
+        __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
+        description = Column(String(1024))
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
+        latitude = Column(Float)
+        longitude = Column(Float)
         amenity_ids = []
 
         reviews = relationship(
-            "Review",
+            'Review',
             backref='state',
             cascade="all, delete, delete-orphan"
         )
 
         amenities = relationship(
-            "Amenity",
+            'Amenity',
             secondary=place_amenity,
             viewonly=False
         )
-
     else:
         city_id = ""
         user_id = ""
@@ -73,10 +72,10 @@ class Place(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db") else object):
         def reviews(self):
             """Attribute reviews that returns
             the list of Review instances"""
+            reviews_id = storage.all('Review')
             from models import storage
-            reviews_id = storage.all('Review').values()
             new_list = []
-            for review in reviews_id:
+            for review in reviews_id.values():
                 if review.place_id == self.id:
                     new_list.append(review)
             return (new_list)
