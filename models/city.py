d2831d3da0dad7ed37316404084563993b18c274
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-08 11:35:04 -0500

    Task 6

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationships, backref
+from sqlalchemy.orm import relationship, backref
 from os import getenv
 
 
@@ -12,7 +12,7 @@ class City(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db")else object):
         __tablename__ = "cities"
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationships(
+        places = relationship(
             "Place",
             cascade="all",
             backref=backref("cities", cascade="all"),
